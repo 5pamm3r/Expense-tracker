580fc48ce074dd5fbb178bbb432bf9bb18efd097
5pamm3r <cmassarino.dev@gmail.com>
2022-10-03 15:23:25 -0300

    Add new header component

--- src/components/Pasivos/index.js
+++ src/components/Pasivos/index.js
@@ -1,17 +1,36 @@
 import React from "react";
 import './Pasivos.css'
+import incomeIcon from '../../assets/icons/income.png';
+import expenseIcon from '../../assets/icons/expense.png'
 
 function Pasivos({ items, children, render, total }) {
+  const date = new Date().toLocaleString('es-ES', {
+    month: 'long',
+    year: 'numeric'
+  })
   return (
     <div className="Pasivos">
+      <div className="Balance__container">
+        <span>CURRENT BALANCE</span>
+        <span>${total}</span>
+        <span>{date}</span>
+      </div>
+      <div className="IncomeExpense__container">
+        <div className="Income__container">
+          <div>
+            <img src={incomeIcon} alt="income" title="Income" />
+          </div>
+          <span>${}</span>
+        </div>
+        <div className="Expense__container">
+          <div>
+            <img src={expenseIcon}  alt="expense" title="Expense"/>
+          </div>
+          <span>${}</span>
+        </div>
+      </div>
       <p className="Title">Registro de gastos</p>
       <div className="Activos__container">
-        <div className="Subtitles__container">
-          <span>Date</span>
-          <span>Description</span>
-          <span>Category</span>
-          <span>Price</span>
-        </div>
         {items.map(render)}
       </div>
       {children}
