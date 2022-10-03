3bbc105cc80c11d5fc4862892b8a44bb5f83720b
5pamm3r <cmassarino.dev@gmail.com>
2022-10-03 15:22:35 -0300

    Edit layaout component

--- src/components/ItemPasivo/index.js
+++ src/components/ItemPasivo/index.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import './ItemPasivo.css'
+import React from "react";
+import "./ItemPasivo.css";
 
-function ItemPasivo({date, detail, category, price}) {
-  
+function ItemPasivo({ image, date, detail, category, price }) {
+  console.log(image);
   return (
-    <div className='Item__container'>
-        <span>{date}</span>
+    <div className="Item__container">
+      <div className="ItemIcon__container">
+        <img src={image} alt="category representation" title={category} />
+      </div>
+      <div className="Detail__container">
         <span>{detail}</span>
-        <span>{category}</span>
-        <span>${price}</span>
+        <span>{date}</span>
+      </div>
+      <span>{category}</span>
+      <span>${price}</span>
     </div>
-  )
+  );
 }
 
-export default ItemPasivo
+export default ItemPasivo;
