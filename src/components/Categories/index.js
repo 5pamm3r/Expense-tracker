f960a8fbfd67333052757868874fc1de33f5c26f
5pamm3r <cmassarino.dev@gmail.com>
2022-10-10 21:06:43 -0300

    Create categories

---
+++ src/components/Categories/index.js
@@ -0,0 +1,51 @@
+import marketIcon from '../../assets/icons/market.png';
+import junkfoodIcon from '../../assets/icons/junkfood.png';
+import personalIcon from '../../assets/icons/personal.png';
+import healthyIcon from '../../assets/icons/healthy.png';
+import clothesIcon from '../../assets/icons/clothes.png';
+import entertainmentIcon from '../../assets/icons/entertainment.png';
+import othersIcon from '../../assets/icons/others.png';
+import salaryIcon from '../../assets/icons/salary.png'
+
+export const CATEGORIES = [
+  {
+    value: "market",
+    count: 0,
+    image: marketIcon,
+  },
+  {
+    value: "junk-food",
+    count: 0,
+    image: junkfoodIcon,
+  },
+  {
+    value: "personal",
+    count: 0,
+    image: personalIcon,
+  },
+  {
+    value: "healthy",
+    count: 0,
+    image: healthyIcon,
+  },
+  {
+    value: "clothes",
+    count: 0,
+    image: clothesIcon,
+  },
+  {
+    value: "entertainment",
+    count: 0,
+    image: entertainmentIcon,
+  },
+  {
+    value: "others",
+    count: 0,
+    image: othersIcon,
+  },
+  {
+    value: 'salary',
+    count: 0,
+    image: salaryIcon
+  }
+];
