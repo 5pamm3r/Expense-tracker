36e6bd46606383a998cda4b09842f6c7f2ca73d4
5pamm3r <cmassarino.dev@gmail.com>
2022-10-10 21:11:53 -0300

    Add effect and income-expense button

--- src/components/NewItem/index.js
+++ src/components/NewItem/index.js
@@ -1,50 +1,93 @@
 import React from "react";
 import "./NewItem.css";
 
-function NewItem({ onSubmit, date, detail, onChangeDetail, onChangeCategory, categories, price, onChangePrice, error }) {
+function NewItem({
+  onSubmit,
+  date,
+  detail,
+  onChangeDetail,
+  onChangeCategory,
+  categories,
+  category,
+  price,
+  onChangePrice,
+  error,
+  onIncome,
+  onExpense,
+}) {
+  React.useEffect(() => {
+    if (category === "salary") {
+      document.getElementById("incomeBtn").disabled = false;
+    } else {
+      document.getElementById("incomeBtn").disabled = true;
+    }
+  }, [category]);
   return (
     <div className="NewItem__container">
       <form onSubmit={onSubmit}>
-        <div className="Date__container">
+        {/* <div className="Date__container">
           <span id="date">{date}</span>
-        </div>
-        <input
-          className="Detail"
-          id="detail"
-          placeholder="Type here..."
-          value={detail}
-          onChange={onChangeDetail}
-          required
-        />
-        <select
-          className="Category"
-          id="category"
-          onChange={onChangeCategory}
-          defaultValue={'DEFAULT'}
-        >
-          <option value="DEFAULT" disabled>Select..</option>
-          <option value={categories[0].value}>{categories[0].value}</option>
-          <option value={categories[1].value}>{categories[1].value}</option>
-          <option value={categories[2].value}>{categories[2].value}</option>
-          <option value={categories[3].value}>{categories[3].value}</option>
-          <option value={categories[4].value}>{categories[4].value}</option>
-          <option value={categories[5].value}>{categories[5].value}</option>
-          <option value={categories[6].value}>{categories[6].value}</option>
-        </select>
-        <div className="Price__container">
-          <span>$</span>
+        </div> */}
+        <div className="Inputs__container">
           <input
-            id="price"
-            placeholder="0"
-            value={price}
-            onChange={onChangePrice}
+            className="Detail"
+            id="detail"
+            placeholder="Type here..."
+            value={detail}
+            onChange={onChangeDetail}
             required
           />
+          <select
+            className="Category"
+            id="category"
+            onChange={onChangeCategory}
+            defaultValue={"DEFAULT"}
+          >
+            <option value="DEFAULT" disabled>
+              Select..
+            </option>
+            <option value={categories[0].value}>{categories[0].value}</option>
+            <option value={categories[1].value}>{categories[1].value}</option>
+            <option value={categories[2].value}>{categories[2].value}</option>
+            <option value={categories[3].value}>{categories[3].value}</option>
+            <option value={categories[4].value}>{categories[4].value}</option>
+            <option value={categories[5].value}>{categories[5].value}</option>
+            <option value={categories[6].value}>{categories[6].value}</option>
+            <option disabled>----------------------</option>
+            <option className="Salary-option" value="salary">
+              Salary
+            </option>
+          </select>
+          <div className="Price__container">
+            <span>$</span>
+            <input
+              id="price"
+              placeholder="0"
+              value={price}
+              onChange={onChangePrice}
+              required
+            />
+          </div>
         </div>
-        <button>Save</button>
+        <div className="SendButtons__container">
+          <button
+            className="Income-button"
+            id="incomeBtn"
+            onClick={onIncome}
+          >
+            Income
+          </button>
+          <button className="Expense-button" onClick={onExpense}>
+            Expense
+          </button>
+        </div>
       </form>
-      {!!error && <p className="Error-category" style={{color: 'red'}}>Select category</p>}
-    </div> 
+      {!!error && (
+        <p className="Error-category" style={{ color: "red" }}>
+          Select category
+        </p>
+      )}
+    </div>
   );
 }
 
