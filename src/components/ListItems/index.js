e368d86cc2593c434afba81934cc591b82f70da2
5pamm3r <cmassarino.dev@gmail.com>
2022-10-22 10:08:53 -0300

    Add new className after click

--- src/components/ListItems/index.js
+++ src/components/ListItems/index.js
@@ -1,26 +1,46 @@
 import React from "react";
 import "./ListItems.css";
 
-function ListItems({ image, date, detail, category, price, status  }) {
-  //Se rompió esto.
-  const changeColor =()=> {
-    if(status === 'income') {
-      return {color:'green'}
-    }else {
-      return {color:'red'}
+function ListItems({
+  image,
+  date,
+  detail,
+  category,
+  price,
+  status,
+  id,
+  onDeleteItems,
+  onRemoveCountCategory,
+}) {
+  const [className, setClassName] = React.useState('')
+
+  const changeColor = () => {
+    if (status === "income") {
+      return { color: "green" };
+    } else {
+      return { color: "red" };
     }
-  }
+  };
+  const onClick = () => {
+    if(className !== ''){
+      setClassName('')
+    } else {
+      setClassName('item-selected')
+    }
+    // onDeleteItems();
+    // onRemoveCountCategory();
+  };
   return (
-    <div className="ListItems__container">
+    <div className={`ListItems__container ${className}`} id={id} onClick={onClick}>
       <div className="ItemIcon__container">
         <img src={image} alt="category representation" title={category} />
       </div>
-      <div className="Detail__container">
-        <span>{detail}</span>
+      <div className="Date__container">
+        <span>{category}</span>
         <span>{date}</span>
       </div>
-      <span>{category}</span>
-      <span style={changeColor()}>${price}</span>
+      <span>{detail}</span>
+      <span style={changeColor()}>${parseFloat(price).toFixed(2)}</span>
     </div>
   );
 }
