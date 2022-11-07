1e8655cabc3ccc04654976c4bb20181af772aeb8
5pamm3r <cmassarino.dev@gmail.com>
2022-11-07 19:07:44 -0300

    Change color

--- src/components/CategoryProgress/index.js
+++ src/components/CategoryProgress/index.js
@@ -3,8 +3,8 @@ import './CategoryProgress.css'
 
 function CategoryProgress({ progressName, progressValue}) {
   const style = {
-    background: `radial-gradient(white 50%, transparent 51%),
-      conic-gradient(green 0deg ${(progressValue / 100) * 360}deg, gainsboro 0deg 0deg)`
+    background: `radial-gradient(#222328 40%, transparent 51%),
+      conic-gradient(#8241f7  0deg, #464da3  ${(progressValue / 100) * 360}deg, gainsboro 0deg, #f9f9f9bf 0deg)`
   }
   return (
     <div className='Progress__circle' style={style}>
