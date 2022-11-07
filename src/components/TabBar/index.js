24864bdd836ff25f7ad08005fb0ae67008b566b5
5pamm3r <cmassarino.dev@gmail.com>
2022-11-07 19:11:40 -0300

    New TabBat component

---
+++ src/components/TabBar/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import homeImg from '../../assets/icons/home.png'
+import plusImg from '../../assets/icons/plus.png'
+import categoryImg from '../../assets/icons/category.png'
+import './TabBar.css'
+function TabBar({setOpenItemModal, activeCategory, setActiveCategory, showGraphics, setShowGraphics}) {
+  const [activeHome, setActiveHome] = React.useState(false);
+  const [activeNewItem, setActiveNewItem] = React.useState(true);
+  const onClickHome = (e) => {
+    setActiveHome(true);
+    setActiveNewItem(false);
+    setActiveCategory(false);
+    if(!!showGraphics) {
+      setShowGraphics(prevState=>!prevState)
+    }
+  }
+  const onClickAdd = () => {
+    setActiveNewItem(true);
+    setActiveCategory(false);
+    setActiveHome(false);
+    if(!!showGraphics) {
+      setShowGraphics(prevState=>!prevState)
+    }
+  }
+  const onOpenModal = () => {
+    if(!activeNewItem){
+      return
+    }
+    setOpenItemModal(prevState=>!prevState)
+
+  }
+  const onClickCategory = () => {
+    setActiveCategory(true);
+    setActiveHome(false);
+    setActiveNewItem(false);
+    setShowGraphics(prevState=>!prevState)
+  }
+  return (
+    <div className="TabBar__container">
+      <ul>
+        <li className={`List ${activeHome}`} onClick={onClickHome}>
+          <a href="#/">
+            <img src={homeImg} alt='home' className='icon'/>
+            <span className="Text">Home</span>
+          </a>
+          <div className={`indicator ${activeHome}`}></div>
+        </li>
+        <li className={`List ${activeNewItem}`} onClick={onClickAdd}>
+          <a href="#/">
+            <img src={plusImg} alt='Add new item' className='icon' onClick={onOpenModal}/>
+            <span className="Text">Add</span>
+          </a>
+          <div className={`indicator ${activeNewItem}`}></div>
+        </li>
+        <li className={`List ${activeCategory}`} onClick={onClickCategory}>
+          <a href="#/">
+            <img src={categoryImg} alt='category' className='icon'/>
+            <span className="Text">Graphics</span>
+          </a>
+          <div className={`indicator ${activeCategory}`}></div>
+        </li>
+      </ul>
+    </div>
+  )
+}
+export default TabBar;
